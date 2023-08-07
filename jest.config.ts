a21774d5cfb91a4a46ed2569d561397b43cf4613
Lucas Santos <lucas201273@gmail.com>
2023-08-07 14:50:08 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  roots: ['./src'], // diretório raiz,
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts, tsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    // see: https://github.com/kulshekhar/ts-jest/issues/414#issuecomment-517944368
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  preset: 'ts-jest',
+}
