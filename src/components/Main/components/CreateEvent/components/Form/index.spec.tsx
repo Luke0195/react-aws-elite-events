fececa9bb80e353ce316f345c751f7f0aaec0a45
Lucas Santos <lucas201273@gmail.com>
2023-08-08 08:07:48 -0300

    test: ensure create event modal start form wwith initial values

--- src/components/Main/components/CreateEvent/components/Form/index.spec.tsx
+++ src/components/Main/components/CreateEvent/components/Form/index.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+
 import { Form } from './index'
 
 describe('Form Component', () => {
@@ -6,5 +7,26 @@ describe('Form Component', () => {
     render(<Form />)
     const heading = screen.getByRole('heading', { level: 2 })
     expect(heading.innerHTML).toBe('Create New Event')
+  })
+
+  it('Should start with form initial values', () => {
+    render(<Form />)
+    const eventName = screen.getByPlaceholderText(
+      'Enter with event name'
+    ) as HTMLInputElement
+    const eventType = screen.getByPlaceholderText(
+      'Select Option'
+    ) as HTMLInputElement
+
+    const submibBtn = screen.getByRole('button', {
+      name: /submit/i,
+    }) as HTMLButtonElement
+    const cancelBtn = screen.getByRole('button', {
+      name: /cancel/i,
+    }) as HTMLButtonElement
+    expect(eventName.value).toBe('')
+    expect(eventType.value).toBe('')
+    expect(submibBtn.disabled).toBe(true)
+    expect(cancelBtn.disabled).toBe(false)
   })
 })
