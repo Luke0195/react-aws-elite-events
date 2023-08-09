ce656677947c0ea40f25cc8c8867d6c015f51a4f
Lucas Santos <lucas201273@gmail.com>
2023-08-09 14:58:22 -0300

    chore: add react-calendar

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,4 +1,6 @@
 import { MainHeader, EventCard } from './components'
+import ReactCalendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
 
 import * as S from './styles'
 import { data } from './mocks/events.json'
@@ -8,7 +10,7 @@ export function Main() {
     <S.Container>
       <div className="flex-1 flex flex-col  px-32   overflow-hidden ">
         <MainHeader />
-        <div className="flex flex-row  bg-blue w-full mt-2">
+        <div className="flex flex-row  bg-blue w-full mt-2 ">
           <div className="h-full bg-red-400 flex-1">
             <div className="grid w-full grid-cols-3 gap-3">
               {data.map((item) => (
@@ -16,7 +18,9 @@ export function Main() {
               ))}
             </div>
           </div>
-          <div className="h-full bg-blue-400 flex-1"> ... </div>
+          <div className="h-full bg-blue-400 flex-1 flex items-center justify-center">
+            <ReactCalendar />
+          </div>
         </div>
       </div>
     </S.Container>
