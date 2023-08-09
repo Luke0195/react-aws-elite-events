8ba064c87ea1d0ccea90177d6937482ddb022e0a
Lucas Santos <lucas201273@gmail.com>
2023-08-09 13:40:55 -0300

    test: ensure event card render a empty string if no eventDate is provided

--- src/components/Main/components/EventCard/event-card.spec.tsx
+++ src/components/Main/components/EventCard/event-card.spec.tsx
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker'
 import { parsedData } from './shared/mapper'
 
 describe('EventCard Component', () => {
-  it('Should render a empty string if no eventId is provided', () => {
+  it('Should render a empty string if no eventName is provided', () => {
     const event = {
       eventId: faker.number.int().toString(),
       eventName: null,
@@ -15,5 +15,18 @@ describe('EventCard Component', () => {
     const parsed = parsedData(event)
     render(<EventCard event={parsed} />)
     expect(parsed.eventName).toBe('-')
+  })
+
+  it('Should render a empty string if no eventDate is provided', () => {
+    const event = {
+      eventId: faker.number.int().toString(),
+      eventName: `Event ${faker.music.songName()}`,
+      eventDate: null,
+      eventType: faker.number.int({ min: 1, max: 4 }),
+    }
+
+    const parsed = parsedData(event)
+    render(<EventCard event={parsed} />)
+    expect(parsed.eventDate).toBe('-')
   })
 })
