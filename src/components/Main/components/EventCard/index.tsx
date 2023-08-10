724436883f9add46530063fca787d5ead81b182c
Lucas Santos <lucas201273@gmail.com>
2023-08-10 11:47:41 -0300

    feat: add persist data on load

--- src/components/Main/components/EventCard/index.tsx
+++ src/components/Main/components/EventCard/index.tsx
@@ -2,7 +2,7 @@ import { getCurrentCardType } from './shared/options/eventStatusOptions'
 import { numberOrDefault } from '@/utils/formatters'
 import { EventProps } from './shared/protocols'
 import { parsedData } from './shared/mapper'
-import { Checkbox } from '@mui/material'
+
 import * as S from './styles'
 
 interface EventCardProps {
@@ -16,7 +16,6 @@ export function EventCard(props: EventCardProps) {
     <S.Container
       cardColor={getCurrentCardType(numberOrDefault(event.eventType))}>
       <div>
-        <Checkbox />
         <strong>Event name: {parsed.eventName} </strong>
       </div>
 
--- src/services/usecases/eventUseCase.tsx
+++ src/services/usecases/eventUseCase.tsx
@@ -1,9 +1,18 @@
 import { Event } from '@/domain/models/event'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export function eventUseCase() {
-  const [events, setEvents] = useState<Event[]>([])
+  const [events, setEvents] = useState<Event[]>(() => {
+    const storagedEvents = localStorage.getItem('@elite-events:events')
+    if (storagedEvents) {
+      return JSON.parse(storagedEvents)
+    }
+    return []
+  })
 
+  useEffect(() => {
+    localStorage.setItem('@elite-events:events', JSON.stringify(events))
+  }, [events])
   const addEvent = (event: Event): void => {
     setEvents((prev) => [...prev, event])
   }
